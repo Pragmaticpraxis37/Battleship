f27c9ba0c00e3b0feb116aa995bdce9627f35182
Adam Bowers <adam.j.bowers@gmail.com>
2020-12-09 08:54:57 -0700

    Create test for consecutive coordinates and method for splitting coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,14 @@
 require "./lib/cell"
 
 class Board
-  attr_reader :cells
+  attr_reader :cells,
+              :consecutive_letters,
+              :consecutive_numbers
 
   def initialize
     @cells = Hash.new
+    @consecutive_numbers = []
+    @consecutive_letters = []
   end
 
   def create_board
@@ -20,24 +24,29 @@ class Board
 
   def valid_length(ship, coordinates)
     ship.length == coordinates.length
-      #valid_placement
   end
 
-  def consecutive_numbers(ship, coordinates)
-    collect = []
+  def split_coordinates(coordinates)
     coordinates.each do |coord|
-      collect << coord[1].to_i
+
+      @consecutive_numbers << coord[1].to_i
+      @consecutive_letters << coord[0].ord
     end
-    tracker = collect[0].to_i
-    return_value = nil
-    collect.each do |number|
-      if number == tracker
-        return_value = true
-        tracker += 1
-      else
-        return_value = false
-      end
-    end
-    return_value
+    # @consecutive_numbers
+    # @consecutive_letters
+    # require "pry"; binding.pry
+
+
+    # consecutive_numbers = nil
+    # consecutive_letters = nil
+    #
+    # collect_numbers.each_cons(2).all? do |a ,b|
+    #   b == a + 1
+    # end
+    #
+    # collect_letters.each_cons(2).all? do |a ,b|
+    #   b == a + 1
+    # end
+
   end
 end
