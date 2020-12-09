0e46ef04f7b68708144d37e8f45d0da87956b07d
Dominic Padula <domopadula2192@gmail.com>
2020-12-09 15:19:15 -0700

    logic for cons numbers

--- lib/board.rb
+++ lib/board.rb
@@ -23,21 +23,29 @@ class Board
       #valid_placement
   end
 
+  
+
   def consecutive_numbers(ship, coordinates)
     collect = []
     coordinates.each do |coord|
       collect << coord[1].to_i
     end
     tracker = collect[0].to_i
-    return_value = nil
-    collect.each do |number|
-      if number == tracker
-        return_value = true
-        tracker += 1
-      else
-        return_value = false
-      end
-    end
-    return_value
+    collect.each_cons(2).all? do |a ,b|
+      b == a + 1
+
+    # return_value = nil
+    # collect.each do |number|
+    #
+    #   if number == tracker
+    #     return_value = true
+    #     tracker += 1
+    #   else
+    #     return_value = false
+    #   end
+    # end
+    # return_value
+  end
+
   end
 end
