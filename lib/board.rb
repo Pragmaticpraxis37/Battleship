982c7cd0d35c4def045d4b1bbf93757069dcd559
Dominic Padula <domopadula2192@gmail.com>
2020-12-09 17:15:57 -0700

    temp save

--- lib/board.rb
+++ lib/board.rb
@@ -26,26 +26,33 @@ class Board
     ship.length == coordinates.length
   end
 
-  def consecutive_numbers(ship, coordinates)
-    collect = []
-    coordinates.each do |coord|
-      collect << coord[1].to_i
-    end
-    tracker = collect[0].to_i
-    collect.each_cons(2).all? do |a ,b|
-      b == a + 1
-
-    # return_value = nil
-    # collect.each do |number|
-    #
-    #   if number == tracker
-    #     return_value = true
-    #     tracker += 1
-    #   else
-    #     return_value = false
-    #   end
+  def valid_consecutive_numbers?
+    # collect = []
+    # coordinates.each do |coord|
+    #   collect << coord[1].to_i
     # end
-    # return_value
+    # tracker = collect[0].to_i
+    answer = consecutive_numbers.each_cons(2).all? do |a ,b|
+      b == a + 1
+    end
+    @consecutive_numbers.clear
+    answer
+  end
+
+  def valid_consecutive_letters?
+    answer = consecutive_letters.each_cons(2).all? do |a ,b|
+      b == a + 1
+    end
+    @consecutive_letters.clear
+    answer
+  end
+
+  def invalid_diagonal
+    if valid_consecutive_letters? && valid_consecutive_numbers? == true
+      false
+    else
+      true
+    end
   end
 
   def split_coordinates(coordinates)
@@ -54,21 +61,18 @@ class Board
       @consecutive_numbers << coord[1].to_i
       @consecutive_letters << coord[0].ord
     end
-    # @consecutive_numbers
-    # @consecutive_letters
-    # require "pry"; binding.pry
+  end
 
-
-    # consecutive_numbers = nil
-    # consecutive_letters = nil
-    #
-    # collect_numbers.each_cons(2).all? do |a ,b|
-    #   b == a + 1
-    # end
-    #
-    # collect_letters.each_cons(2).all? do |a ,b|
-    #   b == a + 1
-    # end
-
+  def valid_placement?(ship, coordinates)
+    split_coordinates(coordinates)
+    if invalid_diagonal == false
+      return false
+    else
+      require "pry"; binding.pry
+    valid_length(ship, coordinates) &&
+    valid_consecutive_numbers? ||
+    valid_length(ship, coordinates) &&
+    valid_consecutive_letters?
+    end
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -41,25 +41,53 @@ class BoardTest < Minitest::Test
   end
 
   def test_split_coordinates_creates_two_arrays
-    @board.create_board
-
     test_letters = @board
-    test = test_letters.split_coordinates(["A1", "A2"])
-    assert_equal [65, 65], test.consecutive_letters
-    assert_equal [1, 2], @board.split_coordinates(["A1", "A2"])
-    # assert_equal false, @board.valid_length(@submarine, ["A2", "A3", "A4"])
+    test_letters.split_coordinates(["A1", "A2"])
+    assert_equal [65, 65], test_letters.consecutive_letters
+    assert_equal [1, 2],  test_letters.consecutive_numbers
+  end
+
+  def test_valid_consecutive_returns_false_or_true
+    test_letters_1 = @board
+    test_letters_1.split_coordinates(["A1", "A2"])
+    test_letters_2 = @board
+    test_letters_2.split_coordinates(["A1", "B1"])
+    assert_equal false, test_letters_1.valid_consecutive_letters?
+    assert_equal true, test_letters_2.valid_consecutive_letters?
 
   end
 
-  # def test_coordinates_are_consecutive
-  #   @board.create_board
-  #   assert_equal false, @board.consecutive_numbers(@cruiser, ["A1", "A2", "A4"])
-  #   assert_equal false, @board.consecutive_numbers(@cruiser, ["A3", "A2", "A1"])
-  #   assert_equal false, @board.consecutive_numbers(@submarine, ["A1", "C1"])
-  #   assert_equal false, @board.consecutive_numbers(@submarine, ["C1", "B1"])
-  #   assert_equal true, @board.consecutive_numbers(@submarine, ["A1", "A2"])
-  #   assert_equal false, @board.consecutive_numbers(@cruiser, ["A1","B2", "C3"])
-  # end
+  def test_consecutive_numbers_can_pass_through_valid_consecutive
+    test_numbers_1 = Board.new
+    test_numbers_1.split_coordinates(["A1", "A2"])
+    test_numbers_2 = Board.new
+    test_numbers_2.split_coordinates(["A1", "B1"])
+    assert_equal true, test_numbers_1.valid_consecutive_numbers?
+    assert_equal false, test_numbers_2.valid_consecutive_numbers?
+  end
+
+  def test_coordinates_cannot_be_diagonal
+    test_diagonal_1 = Board.new
+    test_diagonal_1.split_coordinates(["A1", "B2", "C3"])
+    test_diagonal_1.valid_consecutive_numbers?
+    test_diagonal_1.valid_consecutive_letters?
+    test_diagonal_2 = Board.new
+    test_diagonal_2.split_coordinates(["C2", "D3"])
+    test_diagonal_2.valid_consecutive_numbers?
+    test_diagonal_2.valid_consecutive_letters?
+    assert_equal false, test_diagonal_1.invalid_diagonal
+    assert_equal false, test_diagonal_2.invalid_diagonal
+  end
+
+  def test_valid_placement
+    @board.create_board
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1","B2", "C3"])
+  end
 
 
 end
