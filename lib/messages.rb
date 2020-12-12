d202eb13a46025ee93ab91476fa00b039d33b272
Adam Bowers <adam.j.bowers@gmail.com>
2020-12-12 09:31:14 -0700

    Create test for computer ship placement

--- lib/messages.rb
+++ lib/messages.rb
@@ -6,7 +6,6 @@ require './lib/game'
 class Messages
   def initialize
     @game = Game.new
-
   end
 
   def main_menu
@@ -22,7 +21,32 @@ class Messages
     end
   end
 
+  def player_ship_placement_cruiser
+    puts "I have laid out my ships on the grid.\nYou now need to lay out your two ships.\nThe Cruiser is three units long and the Submarine is two units long.
+  1 2 3 4
+A . . . .
+B . . . .
+C . . . .
+D . . . .
+Enter the squares for the Cruiser (3 spaces):
+>"
+
+  placement = gets.chomp.upcase.split(" ")
+  end
+
+  def player_ship_placement_submarine
+
+    @game.render_player_board 
+
+    puts "Enter the squares for the Submarine (2 spaces):
+  >"
+
+  end
+
   def start_game
-    @game.
+    @game
   end
 end
+
+messages = Messages.new
+require "pry"; binding.pry
