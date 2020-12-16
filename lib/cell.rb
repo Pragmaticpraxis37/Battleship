bdc8be030dc12a0298e179ff34b3d945c97b9fbc
Dominic Padula <domopadula2192@gmail.com>
2020-12-16 13:07:06 -0700

    refactor render for correct board render

--- lib/cell.rb
+++ lib/cell.rb
@@ -28,14 +28,8 @@ class Cell
   def render(default = false)
     return 'S' if default == true && !empty? && @fired_upon == false
     return '.' if @fired_upon == false
-<<<<<<< HEAD
-    return "M" if empty?
-    return "X" if @ship.sunk? && !empty?
-    return "H" if !empty?
-=======
     return 'M' if empty?
     return 'X' if @ship.sunk? && !empty?
     return 'H' unless empty?
->>>>>>> 3b0e0f2420baa4f63180281a5b9bee3a32a20d21
   end
 end
